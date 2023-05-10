84cc53dba026cf1e66b3d3e5644171b05861184f
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-10 19:32:25 +0200

    test

--- nginx-dfunction-module/ngx_http_dfunction_module.c
+++ nginx-dfunction-module/ngx_http_dfunction_module.c
@@ -112,7 +112,7 @@ static ngx_int_t ngx_http_dfunction_handler(ngx_http_request_t *r)
     out.buf = b;
     out.next = NULL; /* just one buffer */
 
-    static u_char ngx_dfunction[] = "data";
+    u_char ngx_dfunction[] = "data";
 
     b->pos = ngx_dfunction; /* first position in memory of the data */
     b->last = ngx_dfunction + sizeof(ngx_dfunction) - 1; /* last position in memory of the data */
