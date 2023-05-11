6da89dea89a128e14b1b9893cf8f0c29a591e329
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-11 16:55:58 +0200

    test

--- compile-module-dfunction.sh
+++ compile-module-dfunction.sh
@@ -7,21 +7,57 @@ NGX_DM=nginx-dfunction-module
 NGX_MN=ngx_http_dfunction_module
 NGX_DR=dfunction
 
+# start the nginx container (see docker-compose.yml for more info.)
 docker compose up -d
 
-docker compose cp ./${NGX_DM}/${NGX_MN}.c nginx:/root/${NGX_DM}/
+# copy code of module to the container
+docker compose cp ./${NGX_DM} nginx:/root/${NGX_DM}
 
-#docker compose exec -it nginx ./configure --with-compat --add-dynamic-module=../${NGX_DM}
+# configure the module
+docker compose exec -it nginx ./configure --with-compat --add-dynamic-module=../${NGX_DM}
 
+# build the module
 docker compose exec -it nginx make modules
 
-# docker compose exec -it nginx cp ./objs/${NGX_MN}.so /etc/nginx/modules/
-
+# copy the compiled module to the current local folder
 docker compose cp nginx:/root/nginx-${NGX_V}/objs/${NGX_MN}.so ./
 
-docker compose stop && docker compose rm -f && docker compose up -d 
+# create the nginx.conf including the load of the .so file (see docker-compose.yml for mount in nignx container)
+cat << EOF > ./nginx.conf
+load_module modules/ngx_http_${NGX_DR}_module.so;
+user  nginx;
+worker_processes  auto;
 
-cat << EOF > ./conf.d/${NGX_DM}.conf
+error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+EOF
+
+# create a new default.conf for nginx (see docker-compose.yml for mount in nignx container)
+cat << EOF > ./conf.d/default.conf
 server {
     listen 80 default_server;
     listen 443 ssl;
@@ -31,14 +67,10 @@ server {
     }
 }
 EOF
-docker compose cp ./conf.d/${NGX_DR}.conf nginx:/etc/nginx/conf.d/
 
-docker compose exec -it nginx nginx -s reload
-
-docker compose logs -f nginx
+# restart the container (see docker-compose.yml for the mount of the current folder .so file)
+docker compose stop && docker compose rm -f && docker compose up -d && docker compose logs -f nginx
 
 
-
-# docker compose exec -it nginx sed -i "1s#^#load_module modules/${NGX_MN}.so;#" /etc/nginx/nginx.conf
-
-# docker compose exec -it nginx nginx -s reload
+# # reload nginx within the container and follow it's logs
+# docker compose exec -it nginx nginx -s reload && docker compose logs -f nginx
